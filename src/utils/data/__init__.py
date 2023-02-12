4c60686d6fad803557aa643ca4eab5a39417a350
Yifan Luo <jeffluoyifan@gmail.com>
2023-02-12 08:54:03 +0000

    update dataloader.py & download.py

--- src/utils/data/__init__.py
+++ src/utils/data/__init__.py
@@ -1,2 +1,4 @@
+print("Running __init__ as {}".format(__name__))
+
 from .download import *
 from .dataloader import *
--- src/utils/data/download.py
+++ src/utils/data/download.py
@@ -1,28 +1,30 @@
 import os
 
-def download_dataset(name:str="cifar10", path:str="."):
-    url = ""
-    if name == "cifar10":
-        url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-    elif name == "imagenet1000":
-        url = "https://www.kaggle.com/datasets/ifigotin/imagenetmini-1000/download?datasetVersionNumber=1"
-    else:
-        print("Invalid dataset name.")
-        return
-    
+def download_dataset(name:str="cifar10", path:str=".", unzip:bool=True, traverse:bool=False):
     full_path = path + "/" + name
     os.system("mkdir -p {}".format(full_path))
 
     if name == "cifar10":
+        url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
         os.system("wget -P {} {}".format(full_path, url))
-        raw_file = full_path + "/" + "cifar-10-python.tar.gz"
-        os.system("tar -xf {} -C {}".format(raw_file, full_path))
+        if unzip:
+            file_path = full_path + "/" + "cifar-10-python.tar.gz"
+            os.system("tar -xf {} -C {}".format(file_path, full_path))
     elif name == "imagenet1000":
+        kaggle_repo = "ifigotin/imagenetmini-1000"
         os.chdir(full_path)
-        os.system("kaggle datasets download ifigotin/imagenetmini-1000")
-        os.system("unzip imagenetmini-1000.zip")
+        os.system("kaggle datasets download {}".format(kaggle_repo))
+        if unzip:
+            os.system("unzip imagenetmini-1000.zip")
+    else:
+        print("Invalid dataset name.")
+        return
     
+    if traverse:
+        for dir_path, dir_names, data_names in os.walk(full_path):
+            print("In {}, there are {} folders and {} files.".format(dir_names, len(dir_names), len(data_names)))
+
 
 if __name__ == "__main__":
-    download_dataset(name="cifar10", path="datasets")
-    download_dataset(name="imagenet1000", path="datasets")
+    download_dataset(name="cifar10", path="datasets", unzip=True, traverse=True)
+    download_dataset(name="imagenet1000", path="datasets", unzip=True, traverse=True)
